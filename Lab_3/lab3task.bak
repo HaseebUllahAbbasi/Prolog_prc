a271c5a5d7b49bc0539e86c2c1f7c6f05b6b4a7e
IT Support Admin <it.support@sibadomain.local>
2021-03-02 00:50:38 +0500

    lab _3

---
+++ Lab_3/lab3task.bak
@@ -0,0 +1,15 @@
+food(chicken).
+color(chicken,brown).
+food(apple).
+color(apple,red).
+food(banana).
+color(banana,yellow).
+food(grapes).
+color(grapes,green).
+food(pineapple).
+color(pineapple,yellow).
+food(bhindi).
+color(bhindi,green).
+
+findcolor(Food,Color):-food(Food),color(Food,Color).
+findfood(Food,Color):-color(Food,Color),food(Food).
--- Lab_3/lab3task.pl
+++ Lab_3/lab3task.pl
@@ -13,3 +13,22 @@ color(bhindi,green).
 
 findcolor(Food,Color):-food(Food),color(Food,Color).
 findfood(Food,Color):-color(Food,Color),food(Food).
+
+person(check,apple).
+person(show,banana).
+person(shaikh,bhindi).
+
+
+
+
+prg :- write('enter food = '),
+	read(Food),nl,
+	findcolor(Food,Color),nl,
+	write('color is '),write(Color),nl.
+prf :- write('enter color = '),
+	read(Color),nl,
+	findfood(Food,Color),nl,
+	write('Food is '),write(Food),nl.
+
+per :- write('enter  name ='),
+	read()
